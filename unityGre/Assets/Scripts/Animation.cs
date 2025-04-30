da86eb240eae10fe47822300e157ff138f9b2865
Greyslor <cdmx2510@amerike.edu.mx>
2025-04-29 20:45:20 -0600

    capas de animacion

---
+++ unityGre/Assets/Scripts/Animation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("speed", Input.GetAxis("Vertical"));
+        if (Input.GetKeyDown(KeyCode.Escape)) 
+        {
+
+        }
+
+
+    }
+}
