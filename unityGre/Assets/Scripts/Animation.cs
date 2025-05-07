9af39c518030a18f625a30f185772afabf65570d
Greyslor <cdmx2510@amerike.edu.mx>
2025-05-06 20:27:27 -0600

    6 mayo 2025 animation IK

--- unityGre/Assets/Scripts/Animation.cs
+++ unityGre/Assets/Scripts/Animation.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class Animation : MonoBehaviour
 {
     public Animator animator;
+    public Transform pointToView;
+    public float rangeHand;
+    
+    public Object libro;
+    public float distancia;
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +23,24 @@ public class Animation : MonoBehaviour
         animator.SetFloat("speed", Input.GetAxis("Vertical"));
         if (Input.GetKeyDown(KeyCode.Escape)) 
         {
-
+            animator.SetTrigger("Attack");
         }
+
+
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        animator.SetLookAtWeight(1f, 1f);
+        animator.SetLookAtPosition(pointToView.position);
+
+        animator.SetLookAtPosition(target.position);
+        //animator.SetLookAtPosition();
+        //animator.
+
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, rangeHand);
+        
+        animator.SetIKPosition(AvatarIKGoal.RightHand, pointToView.position);
 
 
     }
